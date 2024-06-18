48b941b43543ada7571816fb3d7767b7a6bdbe9d
Sheng LIN <linsheng9090@hotmail.com>
2024-06-18 09:33:40 +0200

    add selectors

--- src/libs/cra/stores/cra-stores/src/lib/selectors/core.selectors.ts
+++ src/libs/cra/stores/cra-stores/src/lib/selectors/core.selectors.ts
@@ -1,8 +1,11 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { ICraCoreState, craCoreFeatureKey } from '../reducers/core.reducer';
 import { craEmployeeFeatureKey } from '../reducers/employee.reducer';
+import { craProjectFeatureKey } from '../reducers/project.reducer';
 
 export const selectCraState = createFeatureSelector<ICraCoreState>(craCoreFeatureKey);
 
-export const selectEmployeesState = createSelector(selectCraState, (state: ICraCoreState) => state[craEmployeeFeatureKey]);
+export const selectEmployeeState = createSelector(selectCraState, (state: ICraCoreState) => state[craEmployeeFeatureKey]);
+
+export const selectProjectState = createSelector(selectCraState, (state: ICraCoreState) => state[craProjectFeatureKey]);
 
